8ad1a3815e94489667b5c8a012fea711780bdb34
Caio Cavalcanti <caio.oiac2001@hotmail.com>
2021-11-10 22:10:39 -0300

    adicionando nova rota

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello world!'))
 
+app.get('/conversa', (req, res) => res.send('Estou retornando uma conversa'))
+
 app.listen(process.env.PORT || 3000)
