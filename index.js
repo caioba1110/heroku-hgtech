8edaf484235340ef6047da510087f73c66965d01
TiagoMendes-pixel <62443975+TiagoMendes-pixel@users.noreply.github.com>
2021-11-10 22:23:24 -0300

    meuPrimeiro

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello world!'))
+app.get('/', (req, res) => res.send('Hello Caioba!'))
 
 app.get('/conversa', (req, res) => res.send('Estou retornando uma conversa'))
 
